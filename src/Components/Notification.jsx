420cec9c9211fce2dedf15d1141ae9ef62b5cacb
VakoNarimanidze <119787310+VakoNarimanidze@users.noreply.github.com>
2023-05-12 02:47:38 +0400

    ..

--- src/Components/Notification.jsx
+++ src/Components/Notification.jsx
@@ -1,31 +1,43 @@
-import success from '../../src/Components/images/success.png'
-import error from '../../src/Components/images/error.png'
-import warning from '../../src/Components/images/warning.png'
-import info from '../../src/Components/images/info.png'
+import successCircle from '../../src/Components/images/succes-circle.png'
+import successVector from '../../src/Components/images/seccess-verctor.png'
+import errorCircle from '../../src/Components/images/error-circle.png'
+import errorVector from '../../src/Components/images/error-vector.png'
+import warningCircle from '../../src/Components/images/warning-circle.png'
+import warningVector from '../../src/Components/images/warning-vector.png'
+import infoCircle from '../../src/Components/images/info-circle.png'
+import infoVector from '../../src/Components/images/warning-vector.png'
 import './notification.css'
 
 const BoxItems = [
     {
         id: 1,
-        img: success,
+        before : "",
+        imgBg: successCircle,
+        img :successVector,
         'title': 'Success',
         'text' :'Order Placed Successfully. You can check order delivery status.'
     },
     {
         id: 2,
-        img: error,
+        before : '',
+        imgBg: errorCircle,
+        img :errorVector,
         'title': 'Error',
         'text' :'Order Placed Successfully. You can check order delivery status.'
     },
     {
         id: 3,
-        img: warning,
+        before : '',
+        imgBg: warningCircle,
+        img : warningVector,
         'title': 'Warning',
         'text' :'Order Placed Successfully. You can check order delivery status.'
     },
     {
         id: 4,
-        img: info,
+        before : "",
+        imgBg: infoCircle,
+        img :infoVector,
         'title': 'Info',
         'text' :'Order Placed Successfully. You can check order delivery status.'
     }
@@ -34,18 +46,26 @@ const BoxItems = [
 
 export function Boxes(props) {
     return(
-    <div className="boxContainer">
-        <img src={props.success} alt="" />
+        <div className="boxContainer">
+            <div className="boxes">
+                <div className="beforeDiv">
+                    <div className='before'>{props.before}</div>
+                </div>
+            <div className="relative">
+                <img className='circle' src={props.successCircle} alt="" />
+                <img className='vector' src={props.successVector} alt="" /> 
+            </div>
         <div className="flex">
             <h2>{props.title}</h2>
         <p>{props.text}</p>
         </div>
+            </div>
         </div>
     )
 }
 
 export default function Map() {
     return (
-        (BoxItems.map((item) => (<Boxes title={item.title} text={item.text} success={item.img} /> )) )
+        (BoxItems.map((item) => (<Boxes title={item.title} text={item.text} successCircle={item.img} successVector={item.imgBg} before={ item.before} /> )) )
     )
 }
